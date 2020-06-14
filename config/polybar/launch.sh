06c301eba3d4ab5aca2396faa37405feb63404d7
vibowit <vibowit@vibowit.com>
2020-06-14 20:32:36 +0200

    update

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,7 +7,8 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example >>/tmp/polybar1.log 2>&1 &
+polybar top >>/tmp/polybar1.log 2>&1 &
+polybar bottom >>/tmp/polybar2.log 2>&1 &
 # polybar bar2 >>/tmp/polybar2.log 2>&1 &
 
 echo "Bars launched..."
