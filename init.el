11c169d64b1a7640f4386e0b9160eace74920141
Boguslaw Witkowski <vibowit@vibowit.com>
2021-04-27 20:19:49 +0200

    info how to install modeline fonts

--- init.el
+++ init.el
@@ -295,3 +295,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Install fonts for modeline
+;; M-x all-the-icons-install-fonts
