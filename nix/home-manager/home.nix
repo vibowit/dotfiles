5c441b0c3e1f64f71b8fda3dea63e23930bbfb64
vibowit <vibowit@gmail.com>
2025-01-29 20:50:16 +0100

    working nvim from kickstarter

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -17,27 +17,15 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+  home.packages = with pkgs; [
     # pkgs.cowsay
-    # pkgs.gh
-    # pkgs.neovim
-    pkgs.bat
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    bat
+    gcc
+    ripgrep
+    unzip
+    rsync
+    nerd-fonts.meslo-lg
+    nerd-fonts.jetbrains-mono
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
